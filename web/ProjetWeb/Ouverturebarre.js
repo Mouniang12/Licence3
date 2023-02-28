d0e4879c49616689b42e44203170ea808b3d9516
Mouniang12 <79384304+Mouniang12@users.noreply.github.com>
2023-02-28 15:02:47 +0100

    Delete web/ProjetWeb directory
    Signed-off-by: Mouniang12 <79384304+Mouniang12@users.noreply.github.com>

--- web/ProjetWeb/Ouverturebarre.js
+++
@@ -1,7 +0,0 @@
-function openCloseSearching() {
-    if (document.getElementById("searching").style.display == "none") {
-        document.getElementById("searching").style.display = "flex";
-    } else {
-        document.getElementById("searching").style.display = "none";
-    }
-}
